6ff7d82f0340c532c7d64a45696ad5140a64505d
Ricky Reusser <rsreusser@gmail.com>
2015-05-19 01:59:38 -0400

    Add historical versions + benchmark

--- benchmarks/triangularize.js
+++ benchmarks/triangularize.js
@@ -3,34 +3,55 @@ var bench = require('microbench'),
     ndarray = require('ndarray'),
     pool = require('ndarray-scratch'),
     fill = require('ndarray-fill'),
-    qr = require('../lib'),
-    qrPreUnroll = require('../historical/pre-unroll'),
+    qrv3 = require('../lib'),
+    qrv2 = require('../historical/v2'),
+    qrv1 = require('../historical/v1'),
     show = require('ndarray-show')
 
 bench.usePlugin('clitable');
 
 bench.category('QR factorization',function() {
 
-  var m = 10001,
-      n = 10 
+  var m = 150,
+      n = 60
 
   bench.fixtures('d', pool.zeros([m],'float64'))
   bench.fixtures('A', pool.zeros([m,n],'float64'))
-  bench.fixtures('triv3',qr.triangularize)
-  bench.fixtures('triv2',qrPreUnroll.triangularize)
+  bench.fixtures('x', pool.zeros([n],'float64'))
+  bench.fixtures('b', pool.zeros([m],'float64'))
+  bench.fixtures('work', pool.zeros([qrv1.workVectorLength(m,n)],'float64'))
+  bench.fixtures('triv3',qrv3.triangularize)
+  bench.fixtures('triv2',qrv2.triangularize)
+  bench.fixtures('triv1',qrv1.triangularize)
+  bench.fixtures('solvev3',qrv3.solve)
+  bench.fixtures('solvev2',qrv2.solve)
+  bench.fixtures('solvev1',qrv1.solve)
 
   bench.perTestFixtures('matrix setup',function(fixtures) {
     fill(fixtures.A,function(i,j) {
       return Math.pow(i/(m-1),j)
     })
+    fill(fixtures.x,function(i) {
+      return 1 + Math.sin(i/n)
+    });
+    fill(fixtures.b,function(i) {
+      return 1 + Math.sin(i/n)
+    });
   })
 
-  bench.test('v2',function(fixtures) {
+  bench.test('Naively implemented algorithm',function(fixtures) {
+    fixtures.triv1(fixtures.A, fixtures.work)
+    fixtures.solvev1(fixtures.A, fixtures.work, fixtures.b, fixtures.x)
+  })
+
+  bench.test('Streamlined algorithm',function(fixtures) {
     fixtures.triv2(fixtures.A, fixtures.d)
+    fixtures.solvev2(fixtures.A, fixtures.d, fixtures.b)
   })
 
-  bench.test('v3',function(fixtures) {
+  bench.test('Streamlined + unrolled algorithm',function(fixtures) {
     fixtures.triv3(fixtures.A, fixtures.d)
+    fixtures.solvev3(fixtures.A, fixtures.d, fixtures.b)
   })
 
 }).run()
---
+++ historical/v1.js
@@ -0,0 +1,161 @@
+'use strict';
+
+var blas = require('ndarray-blas-level1'),
+    trsv = require('ndarray-blas-trsv'),
+    pool = require('ndarray-scratch');
+
+//var show = require('ndarray-show');
+
+var triangularize = function triangularize( A, v ) {
+  var i,k,n,m;
+
+  if( A.dimension !== 2 ) {
+    throw new TypeError('triangularize():: dimension of input matrix must be 2.');
+  }
+
+  m = A.shape[0];
+  n = A.shape[1];
+
+  if( m < n ) {
+    throw new TypeError('triangularize():: In input matrix A, number of rows m must be greater than number of column n.');
+  }
+
+  if( v === undefined || (v.shape!==undefined && v.shape[0] < m*n - n*(n-1)/2) ) {
+    throw new TypeError('triangularize():: v must be a vector of length >= m*n-n*(n-1)/2 given A is a matrix of size m x n.');
+  }
+
+  var vlo, x0, sgn, x, nrm, Akmkn, alpha, Akmi;
+
+  for( vlo=0, k=0; k<n; k++ ) {
+
+    // Get this section of the packed output array:
+    x = v.lo(vlo).hi(m-k);
+
+    // v = A[k:m,k]
+    blas.copy( A.pick(null,k).lo(k), x );
+
+    // vk = sign(x1) ||x||_2 e1 + x
+    nrm = blas.nrm2(x);
+    x0 = x.get(0);
+    sgn = x0 < 0 ? -1 : 1;
+    x.set(0, x0 + sgn*nrm);
+
+    // vk = vk / ||vk||_2
+    nrm = blas.nrm2(x);
+    if( nrm === 0 ) return false;
+    blas.scal( 1/nrm, x);
+
+    // A[k:m,k:n] = A[k:n,k:n] - 2 * vk * (vk^* * A[k:m,k:n])
+    
+    // Compute alpha = -2 * vk^* * A[k:m,k:n]:
+    Akmkn = A.lo(k,k);
+
+    for(i=0; i<n-k; i++) {
+      Akmi = Akmkn.pick(null,i);
+      alpha = - 2 * blas.dot(x, Akmi);
+      blas.axpy(alpha, x, Akmi);
+    }
+
+    // Increment the current position in the packed output vector v:
+    vlo += m-k;
+  }
+
+
+  return true;
+};
+
+
+var multiplyByQ = function multiplyByQ ( v, n, x ) {
+  var m, k, vlo, xkm, vk, alpha;
+
+  m = x.shape[0];
+
+  vlo = n*(2*m - n - 1)/2;
+
+  for(k=n-1; k>=0; k--) {
+    xkm = x.lo(k);
+
+    // Get this section of the packed output array:
+    vk = x.lo(k).hi(m-k);
+
+    // calculate -2 * vk' * x[k:m]
+    alpha = - 2 * blas.dot(vk, xkm);
+
+    // Update b[k:m] = b[k:m] - 2 * v * (vk' * b[k:m])
+    blas.axpy( alpha, vk, xkm );
+
+    vlo -= m-k+1;
+  }
+  return true;
+};
+
+var multiplyByQinv = function multiplyByQinv( v, n, b ) {
+  var k,vlo,m,bkm,vk,alpha;
+
+  m = b.shape[0];
+
+  for( vlo=0, k=0; k<n; k++ ) {
+    bkm = b.lo(k);
+
+    // Get this section of the packed output array:
+    vk = v.lo(vlo).hi(m-k);
+
+    // calculate -2 * vk' * b[k:m]
+    alpha = - 2 * blas.dot(vk, bkm);
+
+    // Update b[k:m] = b[k:m] - 2 * v * (vk' * b[k:m])
+    blas.axpy( alpha, vk, bkm );
+
+    // Move to the next section of the packed v array
+    vlo += m-k;
+  }
+
+  return true;
+};
+
+//var constructQ = function( Q, v ) {
+//};
+
+//var factor = function( ) {
+//};
+
+var workVectorLength = function(m,n) {
+  return m*n-n*(n-1)/2;
+};
+
+var workVector = function(m,n,dtype) {
+  console.warn('workVector is deprecated. Please use workVectorLength() and allocate a one-dimensional ndarray of that size instead.')
+  if( dtype === undefined ) {
+    dtype = 'float64';
+  }
+  return pool.zeros([m*n-n*(n-1)/2],dtype);
+};
+
+
+var solve = function( R, v, b, x ) {
+  var m,n;
+
+  if( R.dimension !== 2 ) {
+    throw new TypeError('triangularize():: dimension of input matrix must be 2.');
+  }
+
+  m = R.shape[0];
+  n = R.shape[1];
+
+  multiplyByQinv( v, n, b );
+
+  blas.copy(b.hi(n),x);
+  trsv( R.lo(0,0).hi(n,n), x );
+
+  return true;
+};
+
+
+exports.triangularize = triangularize;
+exports.multiplyByQ = multiplyByQ;
+exports.multiplyByQinv = multiplyByQinv;
+exports.workVectorLength = workVectorLength;
+exports.workVector = workVector;
+//exports.constructQ = constructQ;
+//exports.factor = factor;
+exports.solve = solve;
